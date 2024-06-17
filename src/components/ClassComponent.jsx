bcd1b9b6b652658d97e206696958e9c6a3d39ffe
abhi5733 <abhijeetkale5733@gmail.com>
2024-06-17 11:13:23 +0530

    add

---
+++ src/components/ClassComponent.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class ClassComponent extends Component {
+  render() {
+    return (
+      <div>
+<p>Hi there this is my class component</p>
+
+      </div>
+    )
+  }
+}
---
+++ src/components/FunctionalComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FunctionalComponent = () => {
+  return (
+    <div>
+        <p>This is my functional Component</p>
+    </div>
+  )
+}
+
+export default FunctionalComponent
